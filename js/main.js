801f57c6a632430698db78e4d4d44396ff83cc0b
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-13 16:25:18 +0100

    fixed ticket price issues

--- js/main.js
+++ js/main.js
@@ -2,29 +2,35 @@
 const ticketData = document.getElementById("ticket-data");
 const usernameInput = document.getElementById("username-input");
 const ageInput = document.getElementById("age-input");
-const kmInput = parseInt(document.getElementById("km-input"));
-console.log(typeof kmInput);
-
+const kmInput = document.getElementById("km-input");
 const send = document.getElementById("send-btn");
 const ticket = document.getElementById("outcome");
 const ownersname = document.getElementById("owners-name");
 const fareName = document.getElementById("fare");
 const ticketPriceValue = document.getElementById("ticket-price-value");
+const user = usernameInput.value;
+const ageInfo = ageInput.value;
+
+// ticket.style.display += "none";
 
 // ?? prevengo il comportamento di defasult del form
 ticketData.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(ageInput.value);
+  console.log(kmInput.value);
+  console.log(usernameInput.value);
+  let ticketPrice = kmInput.value * 0.21;
+  console.log("ticketPrice:", ticketPrice.toFixed(2) + "€");
+
+  if ()
+
+
+
+
+
+
 });
 
-const user = usernameInput.value;
-const ageInfo = ageInput.value;
-// const kmNum = parseInt(kmInput.value);
-// console.log(typeof kmInput);
-
 // ?? Faccio sparire la sezione del biglietto in fase di apertura della pagina
-// ticket.style.display += "none";
 // ?? Il programma già chiede all'utente i dati tramite gli imput
 // ?? Procedo al calcolo del prezzo del biglietto sulla base dei km da fare
-let ticketPrice = parseInt((kmInput * 0.21).toFixed(2));
-console.log("ticketPrice:", ticketPrice + "€");
-console.log(typeof ticketPrice);

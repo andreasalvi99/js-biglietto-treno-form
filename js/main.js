a1cc4106f0cbfb75ccfa6f4c2d09e34ef15bb9b9
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-13 14:27:06 +0100

    added prevent default event behaviour

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const ticketData = document.getElementById("ticket-data");
 const usernameInput = document.getElementById("username-input");
 const ageInput = document.getElementById("age-input");
 const kmInput = document.getElementById("km-input");
+const send = document.getElementById("send-btn");
+
+ticketData.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("username:", usernameInput.value);
+  console.log("age:", ageInput.value);
+  console.log("km:", kmInput.value);
+});

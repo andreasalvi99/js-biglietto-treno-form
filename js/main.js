74030a4669094749cd9ce3d6d8a20d82089dbf90
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-13 14:39:56 +0100

    added display none to the ticket section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// ?? Recupero le keyword dall'html
 const ticketData = document.getElementById("ticket-data");
 const usernameInput = document.getElementById("username-input");
 const ageInput = document.getElementById("age-input");
@@ -5,9 +6,13 @@ const kmInput = document.getElementById("km-input");
 const send = document.getElementById("send-btn");
 const ticket = document.getElementById("outcome");
 
+// ?? prevengo il comportamento di defasult del form
 ticketData.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("username:", usernameInput.value);
   console.log("age:", ageInput.value);
   console.log("km:", kmInput.value);
 });
+
+// ?? Faccio sparire la sezione del biglietto in fase di apertura della pagina
+ticket.style.display += "none";
